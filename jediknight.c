ec3f758eab7aec6bd6c10441ab165bff1fb030ca
Ezra <49109859+CodeExia@users.noreply.github.com>
2024-11-21 11:26:35 +0800

    Update jediknight.c
    Added the error stuff

--- jediknight.c
+++ jediknight.c
@@ -3,8 +3,19 @@
 int main()
 
 {
-
+    char name [80];
     int height, age, citizenship, recommendation;
+
+    printf("What is your name?\n");
+    scanf("%s", &name);
+
+    printf("Are you recommended by Jedi Master Obi Wan? (Asnwer in numbers: Y = 1, N = 0)\n");
+    scanf("%d", &recommendation);
+    if (recommendation == 1)
+    {
+        printf("Congrats! %s you are accepted into the Jedi Knight Academy!", name);
+        return 0;
+    }
 
     printf("What is your height?\n");
     scanf("%d", &height);
@@ -15,16 +26,21 @@ int main()
         scanf("%d", &age);
         if (age >= 21 && age <= 25)
         {
-            printf("Are you a citizen of the Planet Endor?\n");
-            scanf("%s", &height);
-            if (height = 'y' || 'Y')
+            printf("Are you a citizen of the Planet Endor? (Asnwer in numbers: Y = 1, N = 0)\n");
+            scanf("%d", &citizenship);
+            if (citizenship == 1)
             {
-                printf("Congrats! You are accepted into the Jedi Knight Academy!");
+                printf("Congrats! %s you are accepted into the Jedi Knight Academy!", name);
             }
-
+            else {
+            printf("Invalid citizenship\n");
+            }
+            return 0;
         }
-        
+        else {
+            printf("Age range is not accepted\n");
+        }
+        return 0;
     }
-
 
 }
