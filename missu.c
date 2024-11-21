1944448c31686451ad2ea1c2dac4cddd6be1b05c
tigcodu <theonlyjimson@gmail.com>
2024-11-21 11:12:03 +0800

    Update missu.c

--- missu.c
+++ missu.c
@@ -13,19 +13,20 @@ scanf("%s", name);
 printf("Height: ");
 scanf("%f", &height);
 printf("Weight: ");
-scanf("&f", &weight);
+scanf("%f", &weight);
 printf("Age: ");
 scanf("%d", &age);
 printf("Educational: ");
 scanf("%d", &educ);
 
+printf("Name: %s", name)
 if (height >= 5)
     if (weight == 50)
         if (age >= 18)
             if (educ == 2 || 3)
-                printf("Bongga!!!");
+                printf("\nBongga!!!");
 else
-    printf("Practice ka muna.");
+    printf("\nPractice ka muna.");
 
 return 0;
 
