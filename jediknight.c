89c93a5c7fb261a849d6fc7387d3ae83b1f8d560
Ezra <49109859+CodeExia@users.noreply.github.com>
2024-11-21 10:59:47 +0800

    Create jediknight.c
    ETO GUYS

---
+++ jediknight.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+    int height;
+    int age;
+    char citizen;
+
+    printf("What is your height?\n");
+    scanf("%d", &height);
+
+    if (height >= 200);
+    {
+        printf("What is your age?\n");
+        scanf("%d", &age);
+        if (age >= 21 && age <= 25)
+        {
+            printf("Are you a citizen of the Planet Endor?\n");
+            scanf("%s", &height);
+            if (height = 'y' || 'Y')
+            {
+                printf("Congrats! You are accepted into the Jedi Knight Academy!");
+            }
+
+        }
+        
+    }
+
+
+}
