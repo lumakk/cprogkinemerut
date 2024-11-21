348ac03e4f10717034b711a29ba1eec63349bfb9
tigcodu <theonlyjimson@gmail.com>
2024-11-21 11:08:35 +0800

    Add files via upload

---
+++ missu.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+char name[80];
+float height;
+float weight;
+int age;
+int educ; // 1 = elem, 2 = hs, 3 = college
+
+printf("Name: ");
+scanf("%s", name);
+printf("Height: ");
+scanf("%f", &height);
+printf("Weight: ");
+scanf("&f", &weight);
+printf("Age: ");
+scanf("%d", &age);
+printf("Educational: ");
+scanf("%d", &educ);
+
+if (height >= 5)
+    if (weight == 50)
+        if (age >= 18)
+            if (educ == 2 || 3)
+                printf("Bongga!!!");
+else
+    printf("Practice ka muna.");
+
+return 0;
+
+}
